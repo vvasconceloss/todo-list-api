92b3bf1a0b22ac9e5bd36a7ef4f128bae2db0e3d
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-26 17:47:52 +0000

    feat(project): add TaskItem repository to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
 using csharp_todolist_api.Context;
 using Microsoft.EntityFrameworkCore;
+using csharp_todolist_api.Interfaces;
+using csharp_todolist_api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
 builder.Services.AddControllers();
+builder.Services.AddScoped<ITaskItemRepository, TaskItemRepository>();
+
 builder.Services.AddDbContext<TodoListContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();

787bd208c1527a789707961a0b8541145c05c1ef
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-26 12:08:28 +0000

    refactor(context): change the name of the task entity and define its builder with OnModelCreating

--- Context/DbContext.cs
+++ Context/DbContext.cs
@@ -1,9 +1,19 @@
+using csharp_todolist_api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace csharp_todolist_api.Context
 {
   public class TodoListContext(DbContextOptions<TodoListContext> options) : DbContext(options)
   {
-    public DbSet<Task> Tasks { get; set; }
-  } 
+    public DbSet<TaskItem> Tasks { get; set; }
+
+    protected void OModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<TaskItem>(entity =>
+      {
+        entity.ToTable("tasks");
+        entity.HasKey(task => task.Id);
+      });
+    }
+  }
 }

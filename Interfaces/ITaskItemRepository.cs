7f6556d8b33ef5179fbacb0611ad87e06cea443d
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-26 20:43:28 +0000

    feat(interfaces): create a method to search for a task by id

--- Interfaces/ITaskItemRepository.cs
+++ Interfaces/ITaskItemRepository.cs
@@ -5,6 +5,7 @@ namespace csharp_todolist_api.Interfaces
   public interface ITaskItemRepository
   {
     public ICollection<TaskItem?> GetTasks();
+    public TaskItem? GetTaskById(int id);
     public TaskItem CreateTask(TaskItem task);
     public void UpdateTask(int id, TaskItem task);
     public void DeleteTask(int id);

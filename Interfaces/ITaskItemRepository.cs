1a6629b3bf3e4c1f604f86c24f348f4d74e54e38
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-26 12:33:47 +0000

    feat(interface): create a repository interface for the task item entity

---
+++ Interfaces/ITaskItemRepository.cs
@@ -0,0 +1,12 @@
+using csharp_todolist_api.Models;
+
+namespace csharp_todolist_api.Interfaces
+{
+  public interface ITaskItemRepository
+  {
+    public ICollection<TaskItem?> GetTasks();
+    public TaskItem CreateTask(TaskItem task);
+    public TaskItem UpdateTask(int id, TaskItem task);
+    public bool DeleteTask(int id);
+  }
+}

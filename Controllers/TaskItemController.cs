2a6188bfbdb46e2799f1b57960be6ae0da30c282
vvasconceloss <vvasconcelos.dev@gmail.com>
2025-11-26 17:51:17 +0000

    feat(controllers): create a route to create tasks

--- Controllers/TaskItemController.cs
+++ Controllers/TaskItemController.cs
@@ -1,10 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using csharp_todolist_api.DTOS;
+using csharp_todolist_api.Models;
+using csharp_todolist_api.Interfaces;
 
 namespace csharp_todolist_api.Controllers
 {
   [ApiController]
-  public class TaskItemController() : ControllerBase
+  public class TaskItemController(ITaskItemRepository taskItemRepository) : ControllerBase
   {
-    
+    private readonly ITaskItemRepository _taskItemRepository = taskItemRepository;
+
+    [HttpPost]
+    [Route("task/create")]
+    public IActionResult Add([FromBody] TaskItemDTO taskItemDTO)
+    {
+      TaskItem taskItem = new()
+      {
+        Title = taskItemDTO.Title,
+        CreatedAt = DateTime.UtcNow,
+        UpdatedAt = DateTime.UtcNow
+      };
+
+      _taskItemRepository.CreateTask(taskItem);
+      
+      return Ok();
+    }
   }
 }
